3f43ba81882b26aabdbe7694379e313d170b2354
steve mavuela <stevemavuela@gmail.com>
2025-03-28 19:57:38 +0100

    app

--- event_news/admin.py
+++ event_news/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Event, News
 
 # Register your models here.
+admin.site.register(Event)
+admin.site.register(News)
--- schedule/admin.py
+++ schedule/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Course, CourseSchedule, Exam, Faculty, Student
 
 # Register your models here.
+admin.site.register(Course)
+admin.site.register(CourseSchedule)
+admin.site.register(Exam)
+admin.site.register(Faculty)
+admin.site.register(Student)
